202b6143e56acd960cfd1bfb9b78dba477e48a81
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-27 14:36:47 -0400

    Initial commit week0 files

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 0; counter < 3; counter = counter + 1)
+            { 
+            Console.WriteLine("This is my first console app.  I did it by myself.");
+            Console.WriteLine("I only know how to writeline.  I think loops are next.");
+            Console.WriteLine("We are moving on...");
+            }
+
+        }
+    }
+}
